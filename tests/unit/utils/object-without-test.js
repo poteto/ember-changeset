fb031f2a507363857c84567e0d4b09174cbafbce
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-07-20 13:27:18 +0200

    More test cleanup

--- tests/unit/utils/object-without-test.js
+++ tests/unit/utils/object-without-test.js
@@ -4,12 +4,12 @@ import { module, test } from 'qunit';
 module('Unit | Utility | object without');
 
 test('it exludes the given keys from all merged objects', function(assert) {
-  let objA = {name: 'Ivan'};
-  let objB = {name: 'John'};
-  let objC = {age: 27};
-  let objD = objectWithout(['age'], objA, objB, objC);
+  let objA = { name: 'Ivan' };
+  let objB = { name: 'John' };
+  let objC = { age: 27 };
+  let objD = objectWithout([ 'age' ], objA, objB, objC);
 
-  assert.deepEqual(objD, {name: 'John'}, 'result only contains name');
+  assert.deepEqual(objD, { name: 'John' }, 'result only contains name');
   assert.deepEqual(objA.name, 'Ivan', 'does not mutate original object');
   assert.deepEqual(objC.age, 27, 'does not mutate original object');
 });
