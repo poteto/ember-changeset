aef210a9d03077f5016994c34ca25ca6a2c48af3
Lauren Tan <laurent@netflix.com>
2016-11-05 01:46:56 +0100

    Changeset helper handles promise as model
    Closes #127

--- addon/helpers/changeset.js
+++ addon/helpers/changeset.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 import Changeset from 'ember-changeset';
 import isChangeset from 'ember-changeset/utils/is-changeset';
+import isPromise from 'ember-changeset/utils/is-promise';
 
 const { Helper: { helper } } = Ember;
 
 export function changeset([obj, validations]) {
   if (isChangeset(obj)) {
     return obj;
+  }
+
+  if (isPromise(obj)) {
+    return obj.then((resolved) => new Changeset(resolved, validations));
   }
 
   return new Changeset(obj, validations);
