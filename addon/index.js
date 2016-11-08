b6d4d086a11da3d2319abafbe8157c046f5d8527
Gabe M <gmarkv10@gmail.com>
2016-11-08 10:41:03 -0500

    Another defensive refactor for update (not just of create)

--- addon/index.js
+++ addon/index.js
@@ -152,8 +152,8 @@ export function changeset(obj, validateFn = defaultValidatorFn, validationMap = 
         let content = get(this, CONTENT);
         let changes = get(this, CHANGES);
         Object.keys(changes).forEach(function(property){
-          let relationship = content.get ? content.get(property) : false;
-          if(relationship){
+          let relationship = content.get ? content.get(property) : false; 
+          if(relationship && typeof relationship.get === 'function'){
             if(relationship.get('content') && relationship.get('content.relationship') && relationship.get('content.relationship.relationshipMeta.kind') === 'hasMany'){
               content.get(property).pushObjects(changes[property]);
               delete changes[property];
