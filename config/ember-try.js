624885e757cafa6d4aa5ad521ca322a8af61776c
Will Raxworthy <me@willrax.com>
2017-04-04 10:26:15 +0100

    update to ember-cli 2.10.1

--- config/ember-try.js
+++ config/ember-try.js
@@ -24,13 +24,24 @@ module.exports = {
       }
     },
     {
-      name: 'ember-lts',
+      name: 'ember-lts-2.4',
       bower: {
         dependencies: {
           "ember": "components/ember#lts-2-4"
         },
         resolutions: {
           "ember": "lts-2-4"
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.8',
+      bower: {
+        dependencies: {
+          "ember": "components/ember#lts-2-8"
+        },
+        resolutions: {
+          "ember": "lts-2-8"
         }
       }
     },
