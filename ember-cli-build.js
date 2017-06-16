e6509c778928e46fac7cded9f2182c704d702cc8
Miguel Camba <miguel.camba@gmail.com>
2017-06-16 17:28:18 +0200

    Remove jQuery and test without it (#185)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,12 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
-    // Add options here
-  });
+  let project = defaults.project;
+  let options = {};
+  if (project.findAddonByName('ember-native-dom-event-dispatcher')) {
+    options.vendorFiles = { 'jquery.js': null };
+  }
+  let app = new EmberAddon(defaults, options);
 
   /*
     This build file specifies the options for the dummy test app of this
