78094713d334449eb0da1d1e6918b32a1c3cee66
Mark David Avery <mark@webark.cc>
2017-08-10 13:39:04 -0700

    Not understanding why travis is throwing a  error for somereson, so trying to get to the base of whats going on

--- testem.js
+++ testem.js
@@ -1,11 +1,22 @@
 /* eslint-env node */
 module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "Chrome"
+  'test_page': 'tests/index.html?hidepassed',
+  'disable_watching': true,
+  'firefox_user_js': './firefox_config.js',
+  'launch_in_ci': [
+    // 'Firefox',
+    'Chrome'
   ],
-  "launch_in_dev": [
-    "Chrome"
-  ]
+  'launch_in_dev': [
+    'Firefox',
+    'Chrome'
+  ],
+  'browser_args': {
+    'Chrome': [
+      '--touch-events',
+      '--disable-gpu',
+      '--headless',
+      '--remote-debugging-port=9222'
+    ]
+  }
 };
