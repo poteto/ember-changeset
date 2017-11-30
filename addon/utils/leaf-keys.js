ab2227cbf89a8633a79f6d3c7a28242f4a3d67d7
Mark David Avery <mark@webark.cc>
2017-11-30 15:27:54 -0800

    refactor(leaf keys): exporting a public method that calls the private method. This is to make the scope variable private to the user

--- addon/utils/leaf-keys.js
+++ addon/utils/leaf-keys.js
@@ -2,24 +2,30 @@ import isObject from 'ember-changeset/utils/is-object';
 import { isArray } from '@ember/array';
 
 /**
- * Given an object, return an array of the leaf-most keys
- * in nested string format.
- *
  * @param  {Object} [object={}] Object to inspect.
  * @param  {String} [scope=''] A string that can be prepended to the object's keys.
- * @return {Array}
+ * @return {Array} Array of the leaf-most keys
  */
-const leafKeys = function(object = {}, scope = '') {
+const _leafKeys = function(object = {}, scope = '') {
   return Object.keys(object).reduce(function(keys, key) {
     const value = object[key];
     let newKeys = [scope + key];
 
     if (isObject(value) && !isArray(value) && Object.keys(value).length) {
-      newKeys = leafKeys(value, scope + key + '.');
+      newKeys = _leafKeys(value, scope + key + '.');
     }
 
     return keys.concat(newKeys);
   }, []);
 }
 
-export default leafKeys;
+/**
+ * Given an object, return an array of the leaf-most keys
+ * in nested string format.
+ *
+ * @param  {Object} [object={}] Object to inspect.
+ * @return {Array} Array of the leaf-most keys
+ */
+export default function leafKeys(object = {}) {
+  return _leafKeys(object);
+}
