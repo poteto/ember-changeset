c2dc3a12d1fa793a1793d62cc12a62fa93ded84f
Kris Handley <handley.kris@gmail.com>
2017-12-06 15:00:10 +0000

    Add jsdoc comment

--- addon/utils/has-own-nested-property.js
+++ addon/utils/has-own-nested-property.js
@@ -1,5 +1,12 @@
 import isObject from './is-object';
 
+/**
+ * Checks if a path exists in an object or any of it's children
+ *
+ * @param {Object} object
+ * @param {String} path
+ * @return {Boolean}
+ */
 export default function hasOwnNestedProperty(object, key) {
   let keyParts = key.split('.');
   let nextKey = keyParts.shift();
