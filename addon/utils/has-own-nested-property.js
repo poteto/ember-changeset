40203b0634fa70f438a76d2a8781b1b6d0d8988c
Kris Handley <handley.kris@gmail.com>
2017-12-08 10:29:24 +0000

    Extract _hasOwnNestedProperty to utils (#234)
    * Extract _hasOwnNestedProperty to utils
    
    * Add jsdoc comment
    
    * Fix hasOwnNestedProperty when the key being tested was the value
    
    Split the previous condition into two early returns to make it clearer
    what is causing the return value.
    
    * Destructure keys
    
    * object hasOwnProperty instead of in object

---
+++ addon/utils/has-own-nested-property.js
@@ -0,0 +1,34 @@
+import isObject from './is-object';
+
+/**
+ * Checks if a path exists in an object or any of it's children
+ *
+ * @param {Object} object
+ * @param {String} path
+ * @return {Boolean}
+ */
+export default function hasOwnNestedProperty(object, key) {
+  let [nextKey, ...keyParts] = key.split('.');
+
+  if (object.hasOwnProperty(nextKey)) {
+    /*
+     * If there are no more keys to test we have reached
+     * the end of the path and the key is present
+    */
+    if (keyParts.length === 0) {
+      return true;
+    }
+
+    /*
+     * If the next part of the object to test is not
+     * an object then don't go any further
+    */
+    if (!isObject(object[nextKey])) {
+      return false;
+    }
+
+    return hasOwnNestedProperty(object[nextKey], keyParts.join('.'));
+  } else {
+    return false;
+  }
+}
