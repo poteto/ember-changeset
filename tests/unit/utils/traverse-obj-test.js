358c3dcb30bf9c697d0f0ef95a04b4920e5865d8
Jason Tu <jasontu4@gmail.com>
2017-12-18 09:54:55 -0500

    Fix prop test

--- tests/unit/utils/traverse-obj-test.js
+++ tests/unit/utils/traverse-obj-test.js
@@ -1,29 +1,45 @@
-import traverse from 'ember-changeset/utils/traverse-obj';
+import traverseObj from 'ember-changeset/utils/traverse-obj';
 import { module, test } from 'qunit';
+import { check, gen, property } from 'ember-changeset/testcheck';
+import { A as emberArray } from '@ember/array';
 
 module('Unit | Utility | traverse obj');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = traverse();
-  console.log(result)
-  assert.ok(result);
+/**
+ * Given a Generator, collect the values of the Generator into an array
+ * and return the array.
+ */
+function collect(gen) {
+  const a = [];
+  for (const v of gen) a.push(v);
+  return a;
+}
 
-  const obj = {
-    foo: {
-      bar: {
-        baz: {
-          whoop: 'dee-doo'
-        }
-      },
+test('returned keys are unique', function(a) {
+  a.expect(100 + 1);
 
-      hello: {
-        world: 42
-      }
-    }
-  };
+  const p = property(gen.JSON, json => {
+    // keys = json
+    //   |> traverseObj
+    //   |> collect
+    //   |> map(o => o.key)
+    //   |> emberArray
+    const pairs = collect(traverseObj(json));
+    const keys = emberArray(pairs.map(p => p.key));
 
-  for (const {key, value} of traverse(obj)) {
-    console.log(key, value);
-  }
+    const actual = keys;
+    const expectedResult = keys.uniq();
+    a.deepEqual(actual, expectedResult);
+  });
+
+  const { result } = check(p);
+  a.ok(result);
+});
+
+test('keys are correct', function(a) {
+  a.expect(0);
+});
+
+test('values are correct', function(a) {
+  a.expect(0);
 });
