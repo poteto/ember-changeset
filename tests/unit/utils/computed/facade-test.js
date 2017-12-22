138bb13afe52d6521ab088e8ac1ab3ece383ca0e
Jason Tu <jasontu4@gmail.com>
2017-12-22 17:38:38 -0500

    Remove a bunch of no longer used utils

--- tests/unit/utils/computed/facade-test.js
+++
@@ -1,8 +0,0 @@
-import facade from 'ember-changeset/utils/computed/facade';
-import { module, test } from 'qunit';
-
-module('Unit | Utility | computed facade');
-
-test("it has no tests", function(assert) {
-  assert.ok(!facade, 'write tests plz');
-});
