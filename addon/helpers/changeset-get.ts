b82df1fecb3a9acecd85f0ea1fc40132de5d190b
Josemar Luedke <josemarluedke@gmail.com>
2019-04-26 11:32:00 -0700

    Add changeset-get helper

---
+++ addon/helpers/changeset-get.ts
@@ -0,0 +1,33 @@
+import Helper from '@ember/component/helper';
+import { observer } from '@ember/object'
+import { ChangesetDef } from 'ember-changeset/types';
+
+const CONTENT = '_content';
+const CHANGES = '_changes';
+
+export default class ChangesetGet extends Helper.extend({
+  invalidate: observer(`changeset.${CONTENT}`, `changeset.${CHANGES}`, function(this: ChangesetGet) {
+    this.recompute();
+  }),
+
+}) {
+  changeset: ChangesetDef | null = null;
+
+  init() {
+    // @ts-ignore
+    super.init(...arguments);
+    this.set('changeset', null)
+  }
+
+  compute(this: ChangesetGet, [changeset, fieldPath]: [ChangesetDef, string]) {
+    if (this.changeset === null) {
+      this.set('changeset', changeset);
+    }
+
+    if (!this.changeset) {
+      return;
+    }
+
+    return this.changeset.get(fieldPath)
+  }
+}
