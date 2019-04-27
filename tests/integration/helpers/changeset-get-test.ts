83176ca66e1641dcdb53441d4d6c0d3608bab042
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2019-04-26 17:39:21 -0700

    Test for changeset-get

--- tests/integration/helpers/changeset-get-test.ts
+++ tests/integration/helpers/changeset-get-test.ts
@@ -1,17 +1,103 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { fillIn, find, settled, render } from "@ember/test-helpers";
+import hbs from "htmlbars-inline-precompile";
 
-module('Integration | Helper | changeset-get', function(hooks) {
+import Changeset from "ember-changeset";
+import { run } from "@ember/runloop";
+
+import { TestContext } from "ember-test-helpers";
+
+interface ModelType {
+  name: { first: string; last: string };
+  email: string;
+  url: string;
+}
+
+module("Integration | Helper | changeset-get", function(hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  let model: ModelType;
 
-    await render(hbs`{{changeset-get inputValue}}`);
+  hooks.beforeEach(function(this: TestContext) {
+    model = {
+      name: {
+        first: "Bob",
+        last: "Loblaw"
+      },
+      email: "bob@lob.law",
+      url: "http://bobloblawslawblog.com"
+    };
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    this.set("changeset", new Changeset(model));
+    this.set("fieldName", "name.first");
+  });
+
+  test("it fails to retrieve the current value using {{get}}", async function(assert) {
+    await render(hbs`
+      <input
+        type="text"
+        oninput={{action (changeset-set changeset fieldName) value="target.value"}}
+        onchange={{action (changeset-set changeset fieldName) value="target.value"}}
+        value={{changeset-get changeset fieldName}}
+      />
+      <p id="test-el">{{get changeset fieldName}}</p>
+      <ul>
+        {{#each (get changeset "changes") as |change|}}
+          <li>{{change.key}}: {{change.value}}</li>
+        {{/each}}
+      </ul>
+    `);
+
+    const input = find("input") as HTMLInputElement;
+    const testEl = find("#test-el");
+
+    try {
+      await fillIn(input!, "Robert");
+
+      assert.equal(testEl!.textContent, "Bob");
+
+      run(() => {
+        this.get("changeset").rollback();
+      });
+
+      assert.equal(testEl!.textContent, "Bob");
+    } catch (e) {
+      assert.ok(false, e.message);
+    }
+  });
+
+  test("it succeeds in retrieving the current value using {{changeset-get}}", async function(assert) {
+    await render(hbs`
+      <input
+        type="text"
+        oninput={{action (changeset-set changeset fieldName) value="target.value"}}
+        onchange={{action (changeset-set changeset fieldName) value="target.value"}}
+        value={{changeset-get changeset fieldName}}
+      />
+      <p id="test-el">{{changeset-get changeset fieldName}}</p>
+      <ul>
+        {{#each (get changeset "changes") as |change|}}
+          <li>{{change.key}}: {{change.value}}</li>
+        {{/each}}
+      </ul>
+    `);
+
+    const input = find("input") as HTMLInputElement;
+    const testEl = find("#test-el");
+
+    try {
+      await fillIn(input!, "Robert");
+
+      assert.equal(testEl!.textContent, "Robert");
+
+      run(() => {
+        this.get("changeset").rollback();
+      });
+
+      assert.equal(testEl!.textContent, "Bob");
+    } catch (e) {
+      assert.ok(false, e.message);
+    }
   });
 });
