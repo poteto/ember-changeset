8bc34b6ca7df2a16d67e829445b793193b6f0199
snewcomer <snewcomer24@gmail.com>
2019-11-22 21:44:03 -0700

    add some tests around normalize obj and pure-assign

---
+++ tests/unit/utils/normalize-object-test.js
@@ -0,0 +1,25 @@
+import normalizeObject from 'ember-changeset/utils/normalize-object';
+import { module, test } from 'ember-qunit';
+
+module('Unit | Utility | normalize object', () => {
+  test('it returns value', async function(assert) {
+    let objA = { value: 'Ivan' };
+    let value = normalizeObject(objA);
+
+    assert.equal(value, 'Ivan', 'result has value');
+  });
+
+  test('it returns value from nested', async function(assert) {
+    let objA = { name: { value: 'Ivan' } };
+    let value = normalizeObject(objA);
+
+    assert.deepEqual(value, { name: 'Ivan' }, 'result has value');
+  });
+
+  test('it returns multiple values from nested', async function(assert) {
+    let objA = { name: { value: 'Ivan' }, foo: { value: 'bar' } };
+    let value = normalizeObject(objA);
+
+    assert.deepEqual(value, { name: 'Ivan', foo: 'bar' }, 'result has value');
+  });
+});
