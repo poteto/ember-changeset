91168683ce3babbf97fd808053e9d1e0a6ff6bb6
snewcomer <snewcomer24@gmail.com>
2019-11-29 08:00:09 -0700

    improve tests

--- addon/helpers/changeset-get.ts
+++ addon/helpers/changeset-get.ts
@@ -2,6 +2,6 @@ import Helper from '@ember/component/helper';
 
 export default class ChangesetGet extends Helper.extend() {
   compute() {
-    throw new Error('changeset-get is now removed as of 3.0.0-beta.0.  See `https://github.com/poteto/ember-changeset/issues/379`');
+    throw new Error('changeset-get is now removed as of 3.0.0-beta.1.  See `https://github.com/poteto/ember-changeset/issues/379`');
   }
 }
