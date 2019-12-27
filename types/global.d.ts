f52c6d7ba01feae9fb56eef5dc6c06494310fdfa
snewcomer <snewcomer24@gmail.com>
2019-12-26 20:47:00 -0800

    try 0.0.8

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,5 @@
 // Types for compiled templates
-declare module 'ember-changeset/templates/*' { 
+declare module 'ember-changeset/templates/*' {
   import { TemplateFactory } from 'htmlbars-inline-precompile';
   const tmpl: TemplateFactory;
   export default tmpl;
