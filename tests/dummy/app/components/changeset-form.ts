ed9bd66467bcc46437b9fd3893c16982d946725a
snewcomer <snewcomer24@gmail.com>
2020-01-10 14:19:52 -0800

    cleanup coponent

--- tests/dummy/app/components/changeset-form.ts
+++ tests/dummy/app/components/changeset-form.ts
@@ -2,12 +2,13 @@ import Component from '@ember/component';
 import { action } from '@ember/object';
 import Changeset from 'ember-changeset'
 
-export default class ChangesetForm extends Component.extend() {
+export default class ChangesetForm extends Component {
   model = {
     email: 'something'
   }
 
   changeset = new Changeset(this.model)
+
   @action submitForm(changeset: any, event: any) {
     event.preventDefault()
     changeset.save()
