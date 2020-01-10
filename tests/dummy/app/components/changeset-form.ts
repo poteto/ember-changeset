58635514a694a7c738880f40ed5b605df218cecb
snewcomer <snewcomer24@gmail.com>
2020-01-10 13:43:42 -0800

    cleanup templates

--- tests/dummy/app/components/changeset-form.ts
+++ tests/dummy/app/components/changeset-form.ts
@@ -1,20 +1,13 @@
 import Component from '@ember/component';
-// @ts-ignore: Ignore import of compiled template
-/* import layout from '../templates/components/changeset-form'; */
-/* import { tracked } from '@glimmer/tracking'; */
 import { action } from '@ember/object';
 import Changeset from 'ember-changeset'
 
-export default class ChangesetForm extends Component.extend({
-  // anything which *must* be merged to prototype here
-}) {
+export default class ChangesetForm extends Component.extend() {
   model = {
     email: 'something'
   }
 
   changeset = new Changeset(this.model)
-  // normal class body definition here
-
   @action submitForm(changeset: any, event: any) {
     event.preventDefault()
     changeset.save()
