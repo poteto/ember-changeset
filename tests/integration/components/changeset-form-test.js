5a9ccee4b35aab1b92d41dbfd2b6cf9c5530abe9
Scott Newcomer <snewcomer@users.noreply.github.com>
2020-01-10 20:20:23 -0800

    Another safeSet for Ember happiness (#397)

--- tests/integration/components/changeset-form-test.js
+++ tests/integration/components/changeset-form-test.js
@@ -8,10 +8,12 @@ module('Integration | Component | changeset-form', function(hooks) {
 
   test('it renders', async function(assert) {
     await render(hbs`<ChangesetForm />`);
-    await fillIn('[data-test-email]', 'foo');
+    await fillIn('[data-test-user-email]', 'foo');
+    await fillIn('[data-test-cid]', 2);
     await click('[data-test-submit]');
 
-    assert.equal(find('[data-test-model-email]').textContent.trim(), 'foo', 'has email after submit');
-    assert.equal(find('[data-test-changeset-email]').textContent.trim(), 'foo', 'changeset has email after submit');
+    assert.equal(find('[data-test-model-cid]').textContent.trim(), '2', 'has email after submit');
+    assert.equal(find('[data-test-model-user-email]').textContent.trim(), 'foo', 'has email after submit');
+    assert.equal(find('[data-test-changeset-user-email]').textContent.trim(), 'foo', 'changeset has email after submit');
   });
 });
