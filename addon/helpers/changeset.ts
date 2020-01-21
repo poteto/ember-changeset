2afc930c1c933d360d300a9688b6e1cab3582acf
snewcomer <snewcomer24@gmail.com>
2020-01-20 16:55:01 -0800

    try reverting for a sec

--- addon/helpers/changeset.ts
+++ addon/helpers/changeset.ts
@@ -1,11 +1,7 @@
 import { helper } from '@ember/component/helper';
-import { Changeset as ChangesetFactory } from 'ember-changeset';
-import {
-  IChangeset,
-  Config,
-  ValidatorAction,
-  ValidatorMap
-} from 'ember-changeset/types';
+import Changeset from 'ember-changeset';
+import { Config } from 'ember-changeset/types/config';
+import { ValidatorAction, ValidatorMap } from 'ember-changeset/types/validator-action';
 import lookupValidator from 'ember-changeset/utils/validator-lookup';
 import isChangeset from 'ember-changeset/utils/is-changeset';
 import isPromise from 'ember-changeset/utils/is-promise';
@@ -14,27 +10,26 @@ import isObject from 'ember-changeset/utils/is-object';
 export function changeset(
   [obj, validations]: [object | Promise<any>, ValidatorAction | ValidatorMap],
   options: Config = {}
-): IChangeset | Promise<IChangeset> {
-  const isIChangeset = (obj: unknown): obj is IChangeset => isChangeset(obj);
-  if (isIChangeset(obj)) {
+): Changeset | Promise<Changeset> {
+  if (isChangeset(obj)) {
     return obj;
   }
 
   if (isObject(validations)) {
     if (isPromise(obj)) {
       return (<Promise<any>>obj).then((resolved) =>
-        ChangesetFactory(resolved, lookupValidator(<ValidatorMap>validations), <ValidatorMap>validations, options)
+        new Changeset(resolved, lookupValidator(<ValidatorMap>validations), <ValidatorMap>validations, options)
       );
     }
 
-    return ChangesetFactory(obj, lookupValidator(<ValidatorMap>validations), <ValidatorMap>validations, options);
+    return new Changeset(obj, lookupValidator(<ValidatorMap>validations), <ValidatorMap>validations, options);
   }
 
   if (isPromise(obj)) {
-    return Promise.resolve(obj).then((resolved: any) => ChangesetFactory(resolved, <ValidatorAction>validations, {}, options));
+    return Promise.resolve(obj).then((resolved: any) => new Changeset(resolved, <ValidatorAction>validations, {}, options));
   }
 
-  return ChangesetFactory(obj, <ValidatorAction>validations, {}, options);
+  return new Changeset(obj, <ValidatorAction>validations, {}, options);
 }
 
 export default helper(changeset);
