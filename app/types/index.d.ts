a8cf88b9348906b66e1d61c0aceeaebd8d4caea5
snewcomer <snewcomer24@gmail.com>
2020-02-12 09:55:41 -0800

    enhancement Add types to app folder as well

---
+++ app/types/index.d.ts
@@ -0,0 +1,20 @@
+import {
+  BufferedChangeset,
+  Changeset,
+  ValidatedChangeset
+} from 'validated-changeset/dist';
+
+import {
+  ValidationResult,
+  ValidatorMapFunc,
+  ValidatorAction
+} from 'validated-changeset/dist/types';
+
+export {
+  BufferedChangeset,
+  Changeset,
+  ValidatedChangeset,
+  ValidationResult,
+  ValidatorMapFunc,
+  ValidatorAction
+};
