34ee47b44b9f33bd3381b1aeeb7a4a0357539280
snewcomer <snewcomer24@gmail.com>
2020-02-27 20:05:07 -0800

    try this

--- config/ember-try.js
+++ config/ember-try.js
@@ -32,6 +32,7 @@ module.exports = async function() {
       },
       {
         name: 'ember-beta',
+        allowedToFail: true,
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('beta')
@@ -40,6 +41,7 @@ module.exports = async function() {
       },
       {
         name: 'ember-canary',
+        allowedToFail: true,
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('canary')
