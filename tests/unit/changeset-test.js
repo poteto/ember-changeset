2b92b9214bdb9870560a4c34d1159938ac742e69
snewcomer <snewcomer24@gmail.com>
2020-03-19 07:02:36 -0700

    fix last test

--- tests/unit/changeset-test.js
+++ tests/unit/changeset-test.js
@@ -486,7 +486,7 @@ module('Unit | Utility | changeset', function(hooks) {
     dummyChangeset.set('dateOfBirth', d);
     let changes = get(dummyChangeset, 'changes');
 
-    assert.equal(model.dateOfBirth, undefined, 'should keep change');
+    assert.ok(model.dateOfBirth, 'should keep change');
     assert.equal(dummyChangeset.get('dateOfBirth'), d, 'should have new change');
 
     assert.deepEqual(changes, expectedChanges, 'should add change');
