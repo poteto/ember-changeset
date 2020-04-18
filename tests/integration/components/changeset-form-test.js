60aed840acc5d6c3096d8af009b7d7c1cef3a6c7
snewcomer <snewcomer24@gmail.com>
2020-04-18 12:31:02 -0700

    Add form isValid/isInvalid states to ensure tracking works

--- tests/integration/components/changeset-form-test.js
+++ tests/integration/components/changeset-form-test.js
@@ -6,7 +6,7 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | changeset-form', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders form', async function(assert) {
     await render(hbs`<ChangesetForm />`);
     await fillIn('[data-test-user-email]', 'foo');
     await fillIn('[data-test-cid]', 2);
@@ -15,27 +15,54 @@ module('Integration | Component | changeset-form', function(hooks) {
     assert.equal(find('[data-test-user-email]').value, 'foo', 'has email input value');
     assert.equal(find('[data-test-user-name]').value, 'someone', 'has name input value');
 
+    assert.equal(find('[data-test-submit]').disabled, true, 'button disabled');
+
+    // submit - disabled until fill in proper email
     await click('[data-test-submit]');
 
-    assert.equal(find('[data-test-model-cid]').textContent.trim(), '2', 'has cid after submit');
-    assert.equal(find('[data-test-model-user-email]').textContent.trim(), 'foo', 'has email after submit');
+    assert.equal(find('[data-test-submit]').disabled, true, 'button disabled');
+    assert.equal(find('[data-test-model-user-email]').textContent.trim(), 'something', 'has old email');
 
-    assert.equal(find('[data-test-changeset-user-email]').textContent.trim(), 'foo', 'changeset has email after submit');
+    await fillIn('[data-test-user-email]', 'foo@gmail.com');
+
+    assert.equal(find('[data-test-submit]').disabled, false, 'button not disabled');
+
+    // submit - now enabled with valid email
+    await click('[data-test-submit]');
+
+    // toggle submit valid state
+    await fillIn('[data-test-user-email]', 'bar ');
+
+    assert.equal(find('[data-test-submit]').disabled, true, 'button disabled');
+
+    await fillIn('[data-test-user-email]', 'foo@gmail.com');
+
+    assert.equal(find('[data-test-submit]').disabled, false, 'button still disabled');
+
+    await fillIn('[data-test-user-name]', 'makers');
+
+    // submit - still enabled
+    assert.equal(find('[data-test-submit]').disabled, false, 'button not disabled');
+
+    assert.equal(find('[data-test-model-cid]').textContent.trim(), '2', 'has cid after submit');
+    assert.equal(find('[data-test-model-user-email]').textContent.trim(), 'foo@gmail.com', 'has email after submit');
+
+    assert.equal(find('[data-test-changeset-user-email]').textContent.trim(), 'foo@gmail.com', 'changeset has email after submit');
 
     assert.equal(find('[data-test-cid]').value, '2', 'still has cid input value');
-    assert.equal(find('[data-test-user-email]').value, 'foo', 'still has email input value');
-    assert.equal(find('[data-test-user-name]').value, 'someone', 'still has name input value');
+    assert.equal(find('[data-test-user-email]').value, 'foo@gmail.com', 'still has email input value');
+    assert.equal(find('[data-test-user-name]').value, 'makers', 'still has name input value');
 
     await fillIn('[data-test-user-name]', 'bar');
 
     assert.equal(find('[data-test-changeset-user-name]').textContent.trim(), 'bar', 'has user name after fill in');
-    assert.equal(find('[data-test-changeset-user-email]').textContent.trim(), 'foo', 'has correct email even when changing related properties');
-    assert.equal(find('[data-test-model-user-email]').textContent.trim(), 'foo', 'has correct email even when changing related properties');
+    assert.equal(find('[data-test-changeset-user-email]').textContent.trim(), 'foo@gmail.com', 'has correct email even when changing related properties');
+    assert.equal(find('[data-test-model-user-email]').textContent.trim(), 'foo@gmail.com', 'has correct email even when changing related properties');
     assert.equal(find('[data-test-changeset-cid]').textContent.trim(), '2', 'has correct cid even when changing related properties');
     assert.equal(find('[data-test-model-cid]').textContent.trim(), '2', 'has correct cid even when changing related properties');
 
     assert.equal(find('[data-test-cid]').value, '2', 'has cid input value');
-    assert.equal(find('[data-test-user-email]').value, 'foo', 'has email input value');
+    assert.equal(find('[data-test-user-email]').value, 'foo@gmail.com', 'has email input value');
     assert.equal(find('[data-test-user-name]').value, 'bar', 'has name input value');
   });
 
