3acfce34401392a9b0cc345e659cfdcb53fa5b8f
scott-newcomer <snewcomer@apple.com>
2020-05-12 15:36:46 -0700

    Safe set for setDeep
    This resolves issues with properties that are consumed in the template

--- addon/index.js
+++ addon/index.js
@@ -19,11 +19,15 @@ export class EmberChangeset extends BufferedChangeset {
   @tracked '_content';
 
   // override base class
+  // DO NOT override setDeep. Ember.set does not work with Ember.set({}, 'user.name', 'foo');
   getDeep = safeGet;
 
   // override base class
   safeGet(obj, key) {
     return safeGet(obj, key);
+  }
+  safeSet(obj, key, value) {
+    return safeSet(obj, key, value);
   }
 
   /**
