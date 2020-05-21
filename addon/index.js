422df58daac0b5a47854fadb4cfd9693e6b8213a
Scott Newcomer <snewcomer24@gmail.com>
2020-05-21 11:05:05 -0700

    Remove #get proxy handler in favor of v-c

--- addon/index.js
+++ addon/index.js
@@ -1,9 +1,6 @@
 import { assert } from '@ember/debug';
-import { BufferedChangeset, normalizeObject } from 'validated-changeset';
+import { BufferedChangeset } from 'validated-changeset';
 import mergeDeep from './utils/merge-deep';
-import isObject from './utils/is-object';
-import { isBelongsToRelationship } from './utils/is-belongs-to-relationship';
-import { isLeafInChanges } from './utils/is-leaf';
 import { notifyPropertyChange } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { get as safeGet, set as safeSet } from '@ember/object';
@@ -118,101 +115,6 @@ export class EmberChangeset extends BufferedChangeset {
     }
 
     return this;
-  }
-
-  /**
-   * Proxied getter
-   * TODO: Evaluate Ember Data particularities here and see how we can use
-   * base class `get`
-   *
-   * @method get
-   */
-  get(key) {
-    // 'person'
-    // 'person.username'
-    let [baseKey, ...remaining] = key.split('.');
-
-    // 1. lets first check CHANGES
-    if (Object.prototype.hasOwnProperty.call(this[CHANGES], baseKey)) {
-      let changes = this[CHANGES];
-      let result;
-
-      if (remaining.length > 0) {
-        let c = changes[baseKey];
-        result = this.getDeep(normalizeObject(c), remaining.join('.'));
-        if (typeof result !== 'undefined') {
-          return result;
-        }
-      } else {
-        result = changes[baseKey];
-      }
-
-      if ((result !== undefined && result !== null) && isObject(result)) {
-        // 1. knock out any class Change{} instances
-        result = normalizeObject(result);
-
-        // 2. then ensure sibling keys are merged with the "result"
-        let content = this[CONTENT];
-
-        // Merge the content with the changes to have a complete object for a nested property.
-        // Given a object with nested property and multiple properties inside of it, if the
-        // requested key is the top most nested property and we have changes in of the properties, we need to
-        // merge the original model data with the changes to have the complete object.
-        // eg. model = { user: { name: 'not changed', email: 'changed@prop.com'} }
-        if (
-          !Array.isArray(result) &&
-          isObject(content[baseKey]) &&
-          !isBelongsToRelationship(content[baseKey]) &&
-          !isLeafInChanges(key, changes)
-        ) {
-          let netKeys = Object.keys(content[baseKey]);
-          if (netKeys.length === 0) {
-            return result;
-          }
-
-          // 3. Ok merge sibling keys.  Yes, shallow clone, but users should treat `c.get` as read only.  Mods to data
-          // structures should happen through `c.set(...)` or `{{changeset-set ...}}`
-          const data = Object.assign(Object.create(Object.getPrototypeOf(content[baseKey])), result);
-          netKeys.forEach(k => {
-            const inResult = this.safeGet(result, k);
-            const contentData = this.getDeep(content, `${baseKey}.${k}`);
-
-            if (
-              isObject(inResult) &&
-              isObject(contentData)
-            ) {
-              data[k] = { ...contentData, ...inResult };
-            } else if (!inResult) {
-              data[k] = contentData;
-            }
-          });
-
-          return data;
-        }
-
-        return result
-      }
-
-      if (result) {
-        return result.value;
-      }
-    }
-
-
-    // 2. return getters/setters/methods on BufferedProxy instance
-    if (typeof this[key] !== 'undefined') {
-      return this[key];
-    } else if (this[baseKey]) {
-      const v = this[baseKey];
-      if (isObject(v)) {
-        const result = this.getDeep(v, remaining.join('.'));
-        return result;
-      }
-    }
-
-    // 3. Lastly return on underlying object if previous cases dont apply
-    const result = this.getDeep(this[CONTENT], key);
-    return result;
   }
 }
 
