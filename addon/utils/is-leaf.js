ea9506fab9fd70e70b3914263092e986a7282267
Andrey Fel <andrey.fel@retailnext.net>
2020-06-03 16:04:28 +0300

    Remove unused util methods (#494)
    It appears these utils are no longer used and can be safely removed.

--- addon/utils/is-leaf.js
+++
@@ -1,6 +0,0 @@
-import { Change } from 'validated-changeset';
-import { get } from '@ember/object';
-
-export function isLeafInChanges(key, changes) {
-  return get(changes, key) && get(changes, key) instanceof Change;
-}
--- addon/utils/is-object.js
+++
@@ -1,7 +0,0 @@
-import { typeOf } from '@ember/utils';
-
-export default function isObject(val) {
-  return val !== null &&
-    !(val instanceof Date) &&
-    (typeof val === 'object' || typeOf(val) === 'instance');
-}
