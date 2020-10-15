7360d817d8c2e982a6e660506e8bf80f98cb3d48
Scott Newcomer <snewcomer24@gmail.com>
2020-10-15 09:45:20 -0500

    add comment;

--- addon/utils/is-object.js
+++ addon/utils/is-object.js
@@ -1,5 +1,9 @@
 import { isArray } from '@ember/array';
 
+/**
+ * Employ Ember strategies for isObject detection
+ * @method isObject
+ */
 export default function isObject(val) {
   return (
     val !== null &&
