dc56ad8b099f1b629d1c9de4c1aaaef0fc225d1e
Scott Newcomer <snewcomer24@gmail.com>
2021-08-13 21:48:24 -0500

    no try

--- config/ember-try.js
+++ config/ember-try.js
@@ -32,14 +32,6 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-release',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release'),
-          },
-        },
-      },
-      {
         name: 'ember-beta',
         allowedToFail: true,
         npm: {
