8fb057d48e4bc696a1e3223ad07fc1e30f56cc57
Julien Palmas <julien@palmas.fr>
2022-02-10 09:28:01 +0100

    🏷️ Add more typings to the `ember-changeset` module
    - export the `EmberChangeset` class
    - export the `changeset` and `Changeset` functions with the extra `changeset` property in the 4th argument of the signature.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,22 @@
-import { Changeset } from 'validated-changeset/dist';
+import { BufferedChangeset } from "validated-changeset";
 
-export { Changeset };
+type BufferedChangesetConstructorParameters = ConstructorParameters<
+  typeof BufferedChangeset
+>;
+
+interface Config extends BufferedChangesetConstructorParameters[3] {
+  changeset: typeof EmberChangeset;
+}
+
+type changesetFunctionsParameters = [
+  BufferedChangesetConstructorParameters[0],
+  BufferedChangesetConstructorParameters[1]?,
+  BufferedChangesetConstructorParameters[2]?,
+  Config?
+];
+
+declare module "ember-changeset" {
+  export class EmberChangeset extends BufferedChangeset {}
+  export function changeset(...args: changesetFunctionsParameters);
+  export function Changeset(...args: changesetFunctionsParameters);
+}
